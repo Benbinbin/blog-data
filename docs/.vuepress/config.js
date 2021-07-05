a4468bd1f0162493db7f46d0a734bc33199d4ed2
Benbinbin <benthomsonbin@gmail.com>
2021-07-05 15:20:58 +0800

    add posts.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,11 @@ const { path } = require('@vuepress/utils')
 module.exports = {
   open: true,
   lang: 'zh-CN',
-  base: "/repo/",
+  base: "/blog-data/",
   title: "Blog",
-  description: 'A blog and knowledge management system.',
+  description: 'A blog and knowledge management system about data and visualization.',
   head: [
-    ['link', { rel: 'icon', href: '/repo/images/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/blog-data/images/favicon.ico' }],
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.13.5/dist/katex.min.css' }],
   ],
   bundler: '@vuepress/vite',
@@ -29,22 +29,34 @@ module.exports = {
     {
       cards: [
         {
-          name: "Folder1",
-          image: "folder1-cover.svg"
+          name: "Anaconda",
+          image: "anaconda.svg"
         },
         {
-          name: "Folder2",
-          image: "folder2-cover.svg"
+          name: "Jupyter",
+          image: "jupyter.svg"
+        },
+        {
+          name: "Math",
+          image: "math.svg"
+        },
+        {
+          name: "Python",
+          image: "python.svg"
+        },
+        {
+          name: "Vis",
+          image: "vis.svg"
         },
       ]
     }],
     [require('./plugins/generateListPages.js'),
     {
-      postClassifications: ['folder1', 'folder2']
+      postClassifications: ['anaconda', 'jupyter', 'math', 'python', 'vis']
     }],
     [require('./plugins/generateFolderPages.js'),
     {
-      postFolders: ['folder1', 'folder2']
+      postFolders: ['anaconda', 'jupyter', 'math', 'python', 'vis']
     }],
   ],
   theme: path.resolve(__dirname, './theme/index.js'),
@@ -64,51 +76,51 @@ module.exports = {
     }
   },
   extendsMarkdown: (md) => {
-    md.use(require('@neilsustc/markdown-it-katex'), {output: 'html'})
+    md.use(require('@neilsustc/markdown-it-katex'), { output: 'html' })
   },
   define: {
-    __BASE__: "/repo/",
-    __HOME_PAGE_TITLE__: "一鱼两吃",
+    __BASE__: "/blog-data/",
+    __HOME_PAGE_TITLE__: "DATA",
     __HOME_PAGE_ICON__: "home_icon.png",
-    __HOME_DESCRIPTION__: "这是我的部落格也是一个知识管理系统。",
-    __HOME_PAGE_COLOR__: '#9CA3AF',
+    __HOME_DESCRIPTION__: "这是我的<a href='https://benbinbin.github.io/tech-blog/' target='_blank' class='hover:text-blue-600 underline font-bold transition-colors'>部落格</a>之一，主要内容是我在学习数据分析和可视化相关过程中做的笔记。",
+    __HOME_PAGE_COLOR__: '#FAE791',
     __AVATAR__: 'avatar.png',
-    __CLASSIFICATIONS__: ['All', 'Folder1', 'Folder2'],
-    __FOLDERS__: ['Folder1', 'Folder2'],
+    __CLASSIFICATIONS__: ['All', 'Anaconda', 'Jupyter', 'Math', 'Python', 'Vis'],
+    __FOLDERS__: ['Anaconda', 'Jupyter', 'Math', 'Python', 'Vis'],
     __FOOTER_AVATAR_LINK__: 'https://github.com/Benbinbin/two-dishes-one-fish',
-    __AUTHOR__: 'two dishes on fish',
+    __AUTHOR__: 'Benbinbin',
     __FOOTER_LICENSE__: 'CC-BY-SA-4.0',
     __FOOTER_LICENSE_LINK__: 'https://creativecommons.org/licenses/by-sa/4.0/deed.en',
     __SOCIAL_MEDIA__: [
       {
         name: 'email',
         logo: 'email.svg',
-        url: 'mailto:[example]@gmail.com'
+        url: 'mailto:benthomsonbin@gmail.com'
       },
       {
         name: 'github',
         logo: 'github.svg',
-        url: 'https://github.com/Benbinbin/two-dishes-one-fish'
+        url: 'https://github.com/Benbinbin'
       },
       {
         name: 'juejin',
         logo: 'juejin.svg',
-        url: 'https://juejin.cn/user/[userid]/posts'
+        url: 'https://juejin.cn/user/3175045314389278/posts'
       },
       {
         name: 'dribbble',
         logo: 'dribbble.svg',
-        url: 'https://dribbble.com/[username]'
+        url: 'https://dribbble.com/BinBinDesign'
       },
       {
         name: 'twitter',
         logo: 'twitter.svg',
-        url: 'https://twitter.com/[username]'
+        url: 'https://twitter.com/Benbinbin_fun'
       },
       {
         name: 'weibo',
         logo: 'weibo.svg',
-        url: 'https://weibo.com/[username]'
+        url: 'https://weibo.com/binbindesign'
       },
     ],
   },
