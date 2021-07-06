714137ee81f3194525187684f0dba563042c1c65
Benbinbin <benthomsonbin@gmail.com>
2021-07-06 13:15:21 +0800

    update home page color.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -94,7 +94,7 @@ module.exports = {
     __HOME_PAGE_TITLE__: "DATA",
     __HOME_PAGE_ICON__: "home_icon.svg",
     __HOME_DESCRIPTION__: "这是我的<a href='https://benbinbin.github.io/tech-blog/' target='_blank' class='hover:text-blue-600 underline font-bold transition-colors'>技术部落格</a>之一，主要内容是我在学习数据分析和可视化相关过程中做的笔记。",
-    __HOME_PAGE_COLOR__: '#FBBF24',
+    __HOME_PAGE_COLOR__: '#9CA3AF',
     __AVATAR__: 'avatar.png',
     __CLASSIFICATIONS__: ['All', 'Anaconda', 'Jupyter', 'Math', 'Python', 'Vis'],
     __FOLDERS__: ['Anaconda', 'Jupyter', 'Math', 'Python', 'Vis'],
